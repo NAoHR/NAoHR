0cb5f9355ff3155c1df84e6072c6e16d0723314f
NAoHR <najmim625@gmail.com>
2022-07-18 21:57:53 +0700

    update: add tech-stack section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,52 @@
 import "../styles/pages/index.css";
 import Navbar from "../components/Navbar";
-import {FaInstagramSquare, FaGithub, FaTwitterSquare, FaTelegram,FaDownload} from "react-icons/fa";
+import {FaInstagramSquare, FaGithub, FaTwitterSquare, FaTelegram,FaDownload, FaLinkedin} from "react-icons/fa";
+
+const dataTech = [
+    {
+        name: "javascript",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/512px-Unofficial_JavaScript_logo_2.svg.png?20141107110902"
+    },
+    {
+        name: "python",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/2048px-Python-logo-notext.svg.png"
+    },
+    {
+        name : "typescript",
+        image: "https://www.saashub.com/images/app/service_logos/11/ad388324edad/large.png?1531424414"
+    },
+    {
+        name : "nodejs",
+        image : "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
+    },
+    {
+        name : "mongodb",
+        image : "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/erkxwhl1gd48xfhe2yld"
+    },
+    {
+        name : "react",
+        image : "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+    },
+    {
+        name : "nextjs",
+        image : "https://seeklogo.com/images/N/next-js-logo-8FCFF51DD2-seeklogo.com.png"
+    },
+    {
+        name : "expressjs",
+        image : "https://tylersharpdeveloper.vercel.app/static/media/express.d638bd34.png"
+    }
+]
+
+function CardImage({image,id}){
+    
+    return (
+        <div className="ts-card">
+            <div className="ts-main">
+                <img src={image} alt="" className="ts-image"/>
+            </div>
+        </div>
+    )
+}
 
 export default function Index() {
     return (
@@ -51,8 +97,13 @@ export default function Index() {
                                 <FaTelegram />
                             </h4>
                         </a>
+                        <a href="https://www.linkedin.com/in/najmi207">
+                            <h4 className="sm-perk c-light">
+                                <FaLinkedin />
+                            </h4>
+                        </a>
                     </div>
-                    <a href="">
+                    <a href="https://docs.google.com/document/d/1AAlO0WLIV2_uVwyOs-jDkFugDJ_7PGBLs-_W4xFGceA/edit?usp=sharing">
                         <div className="download-porto b-purple">
                             <h2 className="dporto-t bebas c-light">
                                 <FaDownload /> Portofolio
@@ -61,7 +112,7 @@ export default function Index() {
                     </a>
                 </div>
             </div>
-            <div className="base-align">
+            <div className="base-align" id="me">
                 <div className="main-title">
                     <h1 className="title-text c-light poppins">
                         <span className="hastag poppins glowing-t">
@@ -94,7 +145,7 @@ export default function Index() {
                     </p>
                 </div>
             </div>
-            <div className="base-align">
+            <div className="base-align" id="stack">
                 <div className="main-title">
                     <h1 className="title-text c-light poppins">
                         <span className="hastag poppins glowing-t">
@@ -108,6 +159,30 @@ export default function Index() {
                         here lies those <span className="glowing-t">
                              <b> tech-stack </b>
                         </span> that i have learnt
+                    </p>
+                </div>
+                <div className="tech-stack-content">
+                    {
+                        dataTech.map((val)=>{
+                            return <CardImage image={val.image} key={val.name} />
+                        })
+                    }
+                </div>
+            </div>
+            <div className="base-align" id="stack">
+                <div className="main-title">
+                    <h1 className="title-text c-light poppins">
+                        <span className="hastag poppins glowing-t">
+                            #
+                        </span>
+                        Project
+                    </h1>
+                </div>
+                <div className="main-text">
+                    <p className="text-m poppins c-light">
+                        Will be added <span className="glowing-t">
+                             <b> Soon </b>
+                        </span>, just sit tight
                     </p>
                 </div>
             </div>
