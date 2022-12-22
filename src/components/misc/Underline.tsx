7f7b264ca44c349c8ef57b49c82675d2abba327f
naohr <najmim625@gmail.com>
2022-12-22 10:03:04 +0700

    refactor: optimize underline

--- src/components/misc/Underline.tsx
+++ src/components/misc/Underline.tsx
@@ -1,17 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 const Underline = ({ mtop="0px"}) => {
-    const [pr, setPr] = useState(100);
+    const elementRef = useRef<HTMLInputElement | null>(null);
 
     useEffect(()=> {
+        
         setInterval(()=> {
-        setPr(Math.floor(Math.random() * 100))
-        }, 1500)
+            if(elementRef.current !== null){
+                elementRef.current.style.width = `${Math.floor(Math.random() * 100)}%`
+            }
+        }, 1300)
+
     }, [])
 
     return (
-        <div className="glowing" style={{
-            width: `${pr}%`,
+        <div className="glowing" id="gl" ref={elementRef} style={{
+            width: `100%`,
             marginTop: mtop,
             height: "3px",
             transition: ".3s"
