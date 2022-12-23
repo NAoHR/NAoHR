e4b8d72e01afee93830e38265b42b97ddce6346c
naohr <najmim625@gmail.com>
2022-12-24 02:54:49 +0700

    update: make theme as context

---
+++ src/contexts/ThemeContexts.tsx
@@ -0,0 +1,68 @@
+import { createContext, ReactNode, useState, Dispatch, SetStateAction } from "react";
+import {MantineProvider} from "@mantine/core";
+
+interface Props {
+    children: ReactNode
+}
+
+type ThemeType = "dark" | "light"
+
+
+export interface ThemeCtxInterface {
+    toggleTheme : Dispatch<SetStateAction<ThemeType>>;
+    currentTheme: "dark" | "light";
+}
+
+
+const defaultProps: ThemeCtxInterface = {
+    toggleTheme : () => {},
+    currentTheme: "dark"
+}
+
+export const ThemeCtx = createContext<ThemeCtxInterface>(defaultProps);
+
+const Theme = (props: Props) => {
+    const [theme, setTheme] = useState<ThemeType>("dark");
+
+    return (
+        <ThemeCtx.Provider value={{toggleTheme: setTheme, currentTheme: theme}}>
+            <MantineProvider withGlobalStyles
+            theme={{
+            colorScheme : theme,
+            globalStyles: (theme) => ({
+                '*, *::before, *::after': {
+                boxSizing: 'border-box',
+                },
+
+                body: {
+                ...theme.fn.fontStyles(),
+                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.dark[4],
+                lineHeight: theme.lineHeight,
+                },
+
+                ".unbounded" : {
+                fontFamily : "Unbounded, cursive;"
+                },
+                ".glowing" : {
+                background: "-webkit-linear-gradient(45deg, #E64980, #4C6EF5)",
+                },
+                ".bold" : {
+                fontWeight: 700
+                },
+                ".glowingT" : {
+                backgroundClip: "text",
+                WebkitBackgroundClip : "text",
+                WebkitTextFillColor: "transparent"
+                }
+
+            }),
+            }}
+            >
+                {props.children}
+            </MantineProvider>
+        </ThemeCtx.Provider>
+    )
+}
+
+export default Theme;
